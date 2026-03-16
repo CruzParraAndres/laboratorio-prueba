d8ad7cb9315000e5559c018dc12f157e1127711a
Andres <boriscruz90@gmail.com>
2026-03-16 11:25:54 -0400

    Subiendo

--- NumeroPar.java
+++ NumeroPar.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
-
 public class NumeroPar {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Introduce un número: ");
-        int num = sc.nextInt();
-        
-        if (num % 2 == 0) {
-            System.out.println("Es par");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número inteiro: ");
+        int numero = scanner.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("O número " + numero + " é par.");
         } else {
-            System.out.println("Es impar");
+            System.out.println("O número " + numero + " é ímpar.");
         }
+
+        scanner.close();
     }
-}
+}   
