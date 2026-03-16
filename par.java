2b7821c2ac4555b965737493720abf363a04b54f
Andres <boriscruz90@gmail.com>
2026-03-16 11:23:40 -0400

    Actualizamos codigo

--- par.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-//Codigo para ver si un numero ingresado es par o no
-System scanner = new Scanner(System.in);
-int numero = scanner.nextInt();
-if (numero % 2 == 0) {
-    System.out.println("El número es par.");
-} else {
-    System.out.println("El número es impar.");
-}
